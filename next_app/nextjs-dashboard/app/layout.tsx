bad6628a69e9f24675da58bb29b6caecda8d17f4
wschenten <william.schenten@kyu.fr>
2025-02-20 14:33:40 +0100

    maj

---
+++ next_app/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css'; // usually import the global.css file on the top level route
+import { inter } from '@/app/ui/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
